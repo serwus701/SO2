68b51b041b8bf4e721aee5e3e0b2451c4f59114c
Michał Malewicz <75280214+serwus701@users.noreply.github.com>
2023-03-27 12:55:24 +0200

    lab1 4.5

--- lab1.sh
+++ lab1.sh
@@ -5,23 +5,52 @@ RM_LIST=${2:-2remove}
 TARGET_DIR=${3:-bakap}
 
 if [ ! -d "$TARGET_DIR" ]; then
-  echo "Katalog $TARGET_DIR nie istnieje, tworzę..."
   mkdir -p "$TARGET_DIR"
 fi
 
-while read -r file_name; do
-  if [ -e "$SOURCE_DIR/$file_name" ]; then
-    echo "Usuwanie pliku/katalogu $file_name..."
-    rm -r "$SOURCE_DIR/$file_name"
-  fi
-done < "$RM_LIST"
+if [ -r "$RM_LIST" ]
+then
+    while read FILE_NAME
+    do
+        if [ -e "$SOURCE_DIR/$FILE_NAME" ]
+        then
+            rm "$SOURCE_DIR/$FILE_NAME"
+        fi
+    done < "$RM_LIST"
+fi
 
-for file in "$SOURCE_DIR"/*; do
+for FILE in "$SOURCE_DIR"/*; do
   if [ -e "$file" ]; then
-    file_name=$(basename "$file")
-    if [ ! -e "$TARGET_DIR/$file_name" ]; then
-      echo "Przenoszenie pliku/katalogu $file_name do $TARGET_DIR..."
-      mv "$file" "$TARGET_DIR"
+    FILE_NAME=$(basename "$FILE")
+    if [ ! -e "$TARGET_DIR/$FILE_NAME" ]; then
+      mv "$FILE" "$TARGET_DIR"
     fi
   fi
 done
+
+for FILE_PATH in "$SOURCE_DIR"/*
+do
+    if [ -f "$FILE_PATH" ]
+    then
+        mv "$FILE_PATH" "$TARGET_DIR"
+    elif [ -d "$FILE_PATH" ]
+    then
+        cp -r "$FILE_PATH" "$TARGET_DIR"
+    fi
+done
+
+REMAINING_FILES_NUM=$(ls "$SOURCE_DIR" | wc -l)
+
+if [ $REMAINING_FILES_NUM -gt 0 ]; then
+     echo "cos zostalo"
+     if [ $REMAINING_FILES_NUM -gt 1 ]; then
+        echo "zostaly co najmniej 2 pliki"
+        if [ $REMAINING_FILES_NUM -gt 3 ]; then
+            echo "zostaly co najmniej 4 pliki"
+        else
+            echo "tutaj tez cos piszemy"
+        fi
+    fi
+else
+    echo "Kononowicz sie zgubil"
+fi
